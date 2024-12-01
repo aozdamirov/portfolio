8bc9004640f9396ad0e76ffb17137132ce586630
aozdamirov <akhmed.ozdamirov30@gmail.com>
2024-12-01 13:05:58 +0100

    Add files via upload

---
+++ AP1 Site associatif/js/demo.js
@@ -0,0 +1,12 @@
+var events = [
+  {'Date': new Date(2023, 10, 7), 'Title': 'Réunion d information' , 'Link': 'https://www.croix-rouge.fr/'},
+  {'Date': new Date(2023, 10, 12), 'Title': 'Mise au point', 'Link': 'https://www.croix-rouge.fr/'},
+  {'Date': new Date(2023, 10, 27), 'Title': 'Stand de collecte de fonds', 'Link': 'https://www.croix-rouge.fr/'},
+  {'Date': new Date(2023, 10, 30), 'Title': 'Formation des nouveaux équipiers ', 'Link': 'https://www.croix-rouge.fr/'},
+  {'Date': new Date(2023, 10, 14), 'Title': 'Formation des fdsfdsfdsfds équipiers ', 'Link': 'https://www.croix-rouge.fr/'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
+
+
